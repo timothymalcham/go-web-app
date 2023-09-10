e19d781ed04e0af59965a61992614236186476cf
Timothy Malcham <timothymalcham@gmail.com>
2023-09-10 16:21:51 +0100

    set up route serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,21 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Static files route
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Application routes
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
